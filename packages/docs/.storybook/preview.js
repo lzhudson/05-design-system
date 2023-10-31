d37d09d7e68e12ae9f31b858f05d78c9c95991ec
Hudson Holanda <hudsonholanda55@gmail.com>
2023-10-31 17:54:23 -0300

    feat: init canvas with background black

--- packages/docs/.storybook/preview.js
+++ packages/docs/.storybook/preview.js
@@ -2,6 +2,9 @@
 const preview = {
   parameters: {
     actions: { argTypesRegex: "^on[A-Z].*" },
+    backgrounds: {
+      default: 'dark'
+    },
     controls: {
       matchers: {
         color: /(background|color)$/i,
