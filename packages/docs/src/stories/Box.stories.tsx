4ad46051a74a1e955b62c8fa0ba54271a36faae4
Hudson Holanda <hudsonholanda55@gmail.com>
2023-12-05 15:25:17 -0300

    feat: create Button component

---
+++ packages/docs/src/stories/Box.stories.tsx
@@ -0,0 +1,16 @@
+import { Box, BoxProps } from '@ignite-ui/react'
+import type { StoryObj, Meta } from '@storybook/react'
+
+export default {
+  title: 'Box',
+  component: Box,
+  args: {
+    children: (
+      <>
+        <span>Testando o elemento Box</span>
+      </>
+    ),
+  },
+} as Meta<BoxProps>
+
+export const Primary: StoryObj<BoxProps> = {}
