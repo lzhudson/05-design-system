738ce44eb8ca6fb24df69861caac55b0ef366121
Hudson Holanda <hudsonholanda55@gmail.com>
2023-12-13 20:40:50 -0300

    fix: add ref to TextInput and adjust styles

--- packages/react/src/components/Button.tsx
+++ packages/react/src/components/Button.tsx
@@ -22,9 +22,15 @@ export const Button = styled('button', {
     width: '$4',
     height: '$4',
   },
+
   '&:disabled': {
     cursor: 'not-allowed',
   },
+
+  '&:focus': {
+    boxShadow: '0 0 0 2px $colors$gray100',
+  },
+
   variants: {
     variant: {
       primary: {
